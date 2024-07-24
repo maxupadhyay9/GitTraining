2f74fb0b69147e11dc6c978dc48a4fc55811e058
Max <Maxupadhyay9@gmail.com>
2024-07-24 20:02:44 +0530

    P user 3rd commit

--- src/Part2.java
+++ src/Part2.java
@@ -5,6 +5,7 @@ public class Part2 {
 		// TODO Auto-generated method stub
 		System.out.println("Part2 1st statement by P User");
 		System.out.println("Part2 1st statement by Q User");
+		System.out.println("Part2 2nd statement by P User");
 	}
 
 }
